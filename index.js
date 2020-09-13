81c38782bc87977c9ba815a22e03bef89c25bdee
Alex <pickeringacw@gmail.com>
2020-09-13 16:48:37 +0200

    Add keywords to selection

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const scraper = require('./scraper.js')
 
 const url = 'https://twitter.com/pickeringacw'
+const keywords = [
+  'potential'
+]
+
+// const url = 'https://masks4you.herokuapp.com/'
+// const keywords = ['R1000', 'customised']
 
 const foo = async () => {
-  const data = await scraper.scrape(url)
+  const data = await scraper.scrape(url, keywords)
   console.log(data)
 }
 
--- scraper.js
+++ scraper.js
@@ -1,24 +1,31 @@
 const puppeteer = require('puppeteer')
 
 const scraper = {
-  scrape: async (url) => {
+  scrape: async (url, keywords) => {
     const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
     await page.goto(url)
-    await page.waitFor(5000)
 
-    const data = await page.evaluate(() => {
-      const tweets = document.querySelectorAll('img')
-      const urls = Array.from(tweets)
-        .map(v => v.src)
+    await page.waitForSelector('[data-testid="tweet"]')
 
-      return {
-        urls
-      }
+    const eval = () => {
+      const cards = document.querySelectorAll('[data-testid="tweet"]')
+      return Array.from(cards)
+        .map(item => item.innerText)
+    }
+
+    const data = await page.evaluate(eval)
+
+    const specificData = []
+    keywords.forEach(word => {
+      specificData.push(data.filter(item => item.toLowerCase()
+        .includes(word.toLowerCase())))
     })
 
     await browser.close()
-    return data
+
+    console.log(specificData, 'data')
+    return specificData
   }
 }
 
